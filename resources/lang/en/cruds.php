1439d07d2500fc8bb7d8e44520c59e9aea774ce9
Sanket Kumar <ssbkumar5513@gmail.com>
2026-06-02 23:36:28 +0530

    sanket

--- resources/lang/en/cruds.php
+++ resources/lang/en/cruds.php
@@ -2,8 +2,8 @@
 
 return [
     'userManagement' => [
-        'title'          => 'Usha Girls Hostel',
-        'title_singular' => 'Usha Girls Hostel',
+        'title'          => 'User management',
+        'title_singular' => 'User management',
     ],
     'permission' => [
         'title'          => 'Permissions',
